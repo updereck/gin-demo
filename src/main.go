38097c8bbe621df3346108c9b71c30806a492e55
董顕華 <updereck@live.cn>
2022-11-26 21:06:29 +0900

    update main.go
    update main .go and task define json

--- src/main.go
+++ src/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,13 +21,32 @@ var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "4", Title: "Sample Data 4", Artist: "Sample User", Price: 60.00},
+}
+
+var env_v1 = os.Getenv("env_variables")
+var env_v2 = os.Getenv("GIN_DEMO_VALUE1")
+var env_v3 = os.Getenv("GIN_DEMO_VALUE2")
+
+var env_v4 = os.Getenv("env_param")
+var env_v5 = os.Getenv("test_ps_value1")
+
+var albums1 = []album{
+	{ID: "1", Title: "Blue 11111", Artist: "John 11111", Price: 56.99},
+	{ID: "env_variables1", Title: env_v1, Artist: env_v2, Price: 0.0},
+	{ID: "env_variables2", Title: env_v1, Artist: env_v3, Price: 0.0},
+}
+
+var albums2 = []album{
+	{ID: "2", Title: "Jeru 2222", Artist: "Gerry 22222", Price: 17.99},
+	{ID: "env_param", Title: env_v4, Artist: env_v5, Price: 0.0},
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/", getAlbums)
-	router.GET("/test", getAlbums)
-	router.GET("/test2", getAlbums)
+	router.GET("/test", getAlbums1)
+	router.GET("/test2", getAlbums2)
 	router.GET("/api", getResponse)
 	router.GET("/api2", getResponse)
 	router.GET("/api3", getResponse)
@@ -55,6 +75,12 @@ func getResponse(c *gin.Context) {
 // getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+func getAlbums1(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums1)
+}
+func getAlbums2(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums2)
 }
 
 // postAlbums adds an album from JSON received in the request body.
